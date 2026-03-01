322d72f982399364c52755ca72aed2e1b5b5832c
Joel Vandenberg <jtvberg@gmail.com>
2026-03-01 13:19:54 -0600

    Initial Commit

---
+++ Appy/AppyApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppyApp.swift
+//  Appy
+//
+//  Created by Joel Vandenberg on 3/1/26.
+//
+
+import SwiftUI
+
+@main
+struct AppyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
